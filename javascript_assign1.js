d0e7e629b52c9f864da1ab9483a42fda5f150140
Olivia Stalcup <ocstalcup@gmail.com>
2017-01-30 14:17:18 -0800

    javascript timer

---
+++ javascript_assign1.js
@@ -0,0 +1,20 @@
+var num1 = 73;
+var num2 = -5;
+var num3 = 100.1;
+var num4 = 2;
+console.log("first number", num1);
+console.log("second number", num2);
+console.log("third number", num3);
+console.log("forth number", num4);
+
+var string1 = "I like to eat, eat, eat"
+var string2 = "apples and bananas!"
+var string3 = "gluten free, non-dairy, free trade."
+console.log("The song goes:", string1);
+console.log("the lyric goes:", string2);
+console.log("last one!", string3);
+
+var boolean1 = true;
+var boolean2 = false;
+console.log("cats are mean", boolean2);
+console.log("I like cats", boolean1);
